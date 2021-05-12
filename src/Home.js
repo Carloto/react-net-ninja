7e6975db45701920fb057b519eb2b3fb7157b5f6
Carloto <viniciuscarloto@gmail.com>
2021-05-12 19:12:25 -0300

    lesson 14

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -7,14 +8,19 @@ const Home = () => {
     {title: 'Response', body:'Yes...', author: 'alien', id: 3}
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
+  useEffect(() => {
+    console.log("effect triggered");
+    console.log(blogs);
+  })
+
   return ( 
     <div className="home">
-      {blogs.map((blog) => 
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      )}
+      <BlogList blogs={blogs} title="All signals" handleDelete={handleDelete} />
     </div>
    );
 }
