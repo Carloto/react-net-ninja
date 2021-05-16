b1846cf44344f6fc09a05b5d9d8a7cdc300d0dab
Carloto <viniciuscarloto@gmail.com>
2021-05-15 21:52:26 -0300

    lesson 26

---
+++ src/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+  return ( 
+    <div className="create">
+      <h2>Send a New Signal</h2>
+    </div>
+   );
+}
+ 
+export default Create;
--- src/useFetch.js
+++ src/useFetch.js
@@ -6,8 +6,10 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url).then(res => {
+      fetch(url, { signal: abortCont.signal }).then(res => {
         if (!res.ok) {
           throw Error('could not fetch the data')
         }
@@ -17,11 +19,17 @@ const useFetch = (url) => {
         setIsPending(false);
         setError(null);
       }).catch(err => {
-        setIsPending(false);
-        setError(err.message);
+        if (err.name === 'AbortError') {
+          console.log('fetch aborted')
+        } else {
+          setIsPending(false);
+          setError(err.message);
+        }
       });
     }, 1000);
-  }, []);
+
+    return () => abortCont.abort();
+  }, [url]);
 
   return { data, isPending, error };
 }
