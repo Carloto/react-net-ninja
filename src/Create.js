80d15018d3ab8886b18442b7ea8a23cda74e54bf
Carloto <viniciuscarloto@gmail.com>
2021-05-16 12:32:12 -0300

    lesson 32 (final lesson)

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,46 @@
+import { useState } from "react";
+import {useHistory} from 'react-router-dom'
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('alien');
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = {title, body, author};
+
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(blog) 
+    }).then(() => {
+      console.log('new signal sent');
+      setIsPending(false);
+      history.push('/');
+    });
+  }
+
   return ( 
     <div className="create">
       <h2>Send a New Signal</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Signal title:</label>
+        <input type="text" required value={title} onChange={e => setTitle(e.target.value)}/>
+        <label>Signal body:</label>
+        <textarea required value={body} onChange={e => setBody(e.target.value)}></textarea>
+        <label>Signal author:</label>
+        <select value={author} onChange={e => setAuthor(e.target.value)}>
+          <option value="me?">me?</option>
+          <option value="alien">alien</option>
+        </select>
+        {!isPending && <button>Send Signal</button>}
+        {isPending && <button disabled>Sending Signal...</button>}
+      </form>
     </div>
    );
 }
